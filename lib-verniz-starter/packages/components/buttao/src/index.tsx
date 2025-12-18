922ee7f7fcebf33dcea5da9570ff3a2c61df9f05
Bruno Camara <bruno_martinez@outlook.com>
2025-12-18 10:58:30 -0300

    feat: new buttao component and add minimatch dependency

---
+++ lib-verniz-starter/packages/components/buttao/src/index.tsx
@@ -0,0 +1,5 @@
+export { Buttao } from './Buttao';
+export type { ButtaoProps } from './Buttao';
+export { buttaoVariants } from './Buttao.styles';
+export type { ButtaoVariants } from './Buttao.styles';
+
---
+++ lib-verniz-starter/packages/components/buttao/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.tsx'],
+  dts: true,
+  format: ['esm', 'cjs'],
+  external: ['react', 'react-dom', 'react/jsx-runtime'],
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  exclude: ['**/*.spec.tsx', '**/*.stories.tsx'],
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.mjs',
+    };
+  },
+});
+
