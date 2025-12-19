bfc082ee49ef14a90d7fa1096eef1e1af2cccae4
str-well <str-well@users.noreply.github.com>
2025-12-19 13:05:58 -0300

    fix: corrige imports do Nav e exclui docs do build no workflow

--- lib-verniz-starter/packages/components/nav/src/Nav.spec.tsx
+++ lib-verniz-starter/packages/components/nav/src/Nav.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import { Nav } from './Nav';
+import { Nav } from './Nav/Nav';
 
 const mockGroups = [
   {
