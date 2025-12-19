47d8c0c80f7db9670cc7b520fe2acc18339aed90
str-well <str-well@users.noreply.github.com>
2025-12-19 13:24:54 -0300

    feat: teste

--- lib-verniz-starter/packages/components/dropdown-menu/src/DropdownMenu.tsx
+++ lib-verniz-starter/packages/components/dropdown-menu/src/DropdownMenu.tsx
@@ -108,7 +108,7 @@ const DropdownMenuCheckboxItem = React.forwardRef<
   >
     <span className="absolute left-2 flex size-3.5 items-center justify-center">
       <DropdownMenuPrimitive.ItemIndicator>
-        {checkIcon || '✓stestes'}
+        {checkIcon || '✓stestses'}
       </DropdownMenuPrimitive.ItemIndicator>
     </span>
     {children}
@@ -133,7 +133,7 @@ const DropdownMenuRadioItem = React.forwardRef<
   >
     <span className="absolute left-2 flex size-3.5 items-center justify-center">
       <DropdownMenuPrimitive.ItemIndicator>
-        {radioIcon || '●testes'}
+        {radioIcon || '●testess'}
       </DropdownMenuPrimitive.ItemIndicator>
     </span>
     {children}
